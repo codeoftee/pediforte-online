460567c76fa6e68cd22b5a75034b78bd46d93ce1
lamiiee <lovezanny22@gmail.com>
2022-06-08 14:29:28 +0100

    routes

--- home/routes.py
+++ home/routes.py
@@ -1,4 +1,6 @@
 from . import home
+from flask import render_template, request, flash, redirect, url_for
+
 
 
 @home.route('/')
@@ -7,30 +9,25 @@ def home_page():
 
 
 @home.route('/sign-up')
-<<<<<<< HEAD
-def sign-up():
-    return render_template('sign-up.html')  
+def sign_up():
+    return render_template("sign-up.html")
 
 @home.route('/do-sign-up', methods=['POST'])
-def do_signup():
-    firstname = request.form['firstname']
-    lastname = request.form['lastname']
+def do_sign_up():
     username = request.form['username']
     email = request.form['email']
     phone = request.form['phone']
     password = request.form['password']
     password2 = request.form['password2']
-    if firstname == '' or lastname == '' or email == ''or password == '' or username == '':
+    if  email == '' or password == '' or username == '' or phone == '':
         flash('All fields are required! ')
-        return redirect(url_for('register_page'))
+        return redirect(url_for('sign_up'))
     if password != password2:
         flash('Password does not match')
-        return redirect(url_for('register_page'))
+        return redirect(url_for('sign_up'))
     password_hash = hashlib.sha256(password.encode()).hexdigest()
-=======
-def sign_up():
-    return "sign up"
->>>>>>> e02b4b4ec4188a33d2c06a87d35bed13fd0c8cf8
+
+    
 
 
 @home.route('/login')
